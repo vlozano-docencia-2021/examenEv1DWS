8caf9396061cd38f31fa506cde3390159f5f36ec
vlozano <vlozano.docencia@gmail.com>
2022-12-02 18:38:08 +0100

    Ejercicio 3.c resuelto

--- cabecera.php
+++ cabecera.php
@@ -1,7 +1,26 @@
 <header>
  Usuario: <?php echo $_SESSION['usuario']['correo']?>
+ <?php 
+	$rol = $_SESSION["usuario"]["Rol"];
+	if($rol==0){
+?>
  <a href="categorias.php">Home</a>
  <a href="carrito.php">Ver carrito</a> 
+<?php
+	}
+	if($rol==1){
+?>
+ <a href="pedidos.php">Ver Pedidos</a>
+<?php
+	}
+	if($rol==2){
+?>
+ <a href="restaurantes.php">Ver Restaurantes</a>
+<?php
+	}
+?>
+ 
+ 
  <a href="logout.php">Cerrar sesión</a>
 </header>
 <hr>
